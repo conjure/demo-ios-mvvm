bb8782fe07039cae4b7904a17ff40471e1e7fa2a
Ade Adegoke <ade.adegoke@conjure.co.uk>
2023-03-14 15:07:07 +0000

    Added the bus stop and arrival time domains

---
+++ BusTimes-MVVM/Source/Domain/BusStop.swift
@@ -0,0 +1,32 @@
+//
+//  BusStop.swift
+//  BusTimes-MVVM
+//
+//  Created by Ade Adegoke on 05/02/2023.
+//
+
+import Foundation
+
+struct TravelInformation: Codable {
+    var stopPoints: [BusStop]
+}
+
+extension TravelInformation: Equatable {}
+
+struct BusStop: Codable, Hashable  {
+    var id: String
+    
+    var naptanId: String
+    var commonName: String
+    var lines: [Lines]
+}
+
+extension BusStop: Identifiable {
+    var identifier: String {
+        return id
+    }
+}
+
+struct Lines: Codable, Hashable {
+    var name: String
+}
