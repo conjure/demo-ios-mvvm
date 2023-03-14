1bab7168722b3e6b8fe9e65c5b74de3da867c12a
Ade Adegoke <ade.adegoke@conjure.co.uk>
2023-03-14 15:03:06 +0000

    Created project and removed unnecessary files

---
+++ BusTimes-MVVM/Source/BusTimes_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusTimes_MVVMApp.swift
+//  BusTimes-MVVM
+//
+//  Created by Ade Adegoke on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BusTimes_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
