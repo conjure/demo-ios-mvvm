2ee9795c34c98da1fa1ecc95e2d0b15bb0745a1e
Ade Adegoke <ade.adegoke@conjure.co.uk>
2023-03-14 15:12:08 +0000

    Added the arrival time views and model

---
+++ BusTimes-MVVM/Source/View/ArrivalTimeView/ArrivalTimeView.swift
@@ -0,0 +1,39 @@
+//
+//  ArrivalTimeView.swift
+//  BusTimes-MVVM
+//
+//  Created by Ade Adegoke on 12/02/2023.
+//
+
+import SwiftUI
+
+struct ArrivalTimeView: View {
+    @StateObject private var viewModel = ArrivalTimeViewModel()
+    var busTopID: String
+    
+    init(busTopID: String) {
+        self.busTopID = busTopID
+    }
+    
+    var body: some View {
+        
+        VStack {
+            Text("Arrival Times")
+                .font(.system(size: 36, weight: .black))
+                .padding(.top, 100)
+            ScrollView(.vertical) {
+                LazyVStack(spacing: 0) {
+                    ForEach(0..<viewModel.arrivalTime.count, id: \.self) { i in
+                        let arrivalTime = viewModel.arrivalTime[i]
+                        ArriveTimeRow(arrivalTime: arrivalTime)
+                        
+                    }
+                }
+            }
+            .padding(.top, 10)
+            .onAppear {
+                viewModel.getBussesArivalTimes(for: busTopID)
+            }
+        }
+    }
+}
